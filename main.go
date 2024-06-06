b77160ee66b15092446dc7216cc87c8d3f2ddf63
wiwi.juwita <wiwijuwita105@gmail.com>
2024-06-06 11:36:16 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"session1-intorduction-gin/middleware"
+	"session1-intorduction-gin/router"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Inisialisasi router Gin
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	r.Use(middleware.AuthMiddleware())
+
+	// Definisikan route
+	router.SetupRouter(r)
+
+	// Jalankan server pada port 8080
+	log.Println("Running server on port 8080")
+	r.Run(":8080")
+}
